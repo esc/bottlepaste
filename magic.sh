147f909186f10b7e8ae4e6d25560ddb966ba85eb
Valentin Haenel <valentin.haenel@gmx.de>
2012-11-01 21:47:36 +0100

    autogenerate index and deliver it

--- magic.sh
+++ magic.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 set +x
-
+rst2html.py --stylesheet-path=bottlepaste.css,github.css  index.rst index.tpl &&
+git add index.tpl &&
+git commit -m "Autocommit index.tpl"
 git push github master &&
 git co custom &&
 git rebase master &&
