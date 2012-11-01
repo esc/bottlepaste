7397cfafcd654a8f72e7bac540262bed103630ad
Valentin Haenel <valentin.haenel@gmx.de>
2012-11-01 22:12:00 +0100

    and god said: let there be rebase

--- magic.sh
+++ magic.sh
@@ -4,6 +4,6 @@ set +x
 rst2html.py --stylesheet-path=bottlepaste.css,github.css  index.rst index.tpl &&
 git add index.tpl &&
 git commit -m "Autocommit index.tpl"
-git push github master &&
-git push cloud master &&
+git push -f github master &&
+git push -f cloud master &&
 cctrlapp bottlepaste/default deploy
